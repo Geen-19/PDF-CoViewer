ae330731c88156e8e9c3de0237559026b59e5006
geen <angelinedias53@gmail.com>
2024-11-12 16:00:20 +0530

    logic change

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ const PDFCoViewer = () => {
           <div className="flex items-center space-x-4">
             <div className="flex items-center space-x-2">
               <Users className="w-5 h-5 text-gray-600" />
-              <span>{activeUsers} viewers</span>
+              <span>{activeUsers-1} viewers</span>
             </div>
             {isAdmin && (
               <div className="flex items-center space-x-2">
